9b83494c2259312aedcd093c96c99dfe3e4f839c
Nick Wood <nickwood@hey.com>
2020-08-08 12:33:53 -0700

    add some systemd deployment stuff

---
+++ deploy/scripts/chronocam-video.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# make sure there's a place to store video output
+if [ ! -d /opt/chronocam-video-output ]; then
+    mkdir -p /opt/chronocam-video-output
+fi
+
+# move current photos off to their own directory so the camera service
+# can keep saving photos
+if [ ! -d "/opt/chronocam-video-work" ]; then
+    mkdir -p /opt/chronocam-video-work
+    mv /opt/chronocam-output/*.jpg /opt/chronocam-video-work
+fi
+
+# use ffmpeg to produce the video
+echo "processing timelapse video"
+ffmpeg \
+    -r 24 -pattern_type \
+    glob -i '/opt/chronocam-video-work/*.jpg' \
+    -s hd1080 \
+    -vcodec libx264 \
+    "/opt/chronocam-video/$(date +"%m_%d_%Y").mp4"
+
+# we are done, so we can remove the working directory and reclaim the storage
+# space occupied by the JPEG files
+echo "removing chronocam video working directory"
+rm -rf /opt/chronocam-video-work
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd "${DIR}"
+echo "building chronocam release"
+cargo build --release
+popd
+
+# this script currently assumes you're running on a raspberry pi
+# with raspbian running
+echo "installing chronocam release"
+cp "${DIR}/target/release/chronocam" /usr/local/bin
+
+echo "installing scripts"
+cp "${DIR}/deploy/scripts/*.sh" /usr/local/bin
+
+echo "installing systemd units"
+cp "${DIR}/deploy/systemd/*.service" /etc/systemd/system/
+cp "${DIR}/deploy/systemd/*.timer" /etc/systemd/system/
+
+echo "starting units"
+systemctl enable chronocam.service
+systemctl start chronocam.service
+
+systemctl enable chronocam-video.timer
